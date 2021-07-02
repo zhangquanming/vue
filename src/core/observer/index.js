6fe8107fee1bc03b70570e603b90057395885efd
zhangquanming <419654548@qq.com>
2021-07-02 19:34:04 +0800

    注释标记

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -29,10 +29,8 @@ export function toggleObserving (value: boolean) {
 }
 
 /**
- * Observer class that is attached to each observed
- * object. Once attached, the observer converts the target
- * object's property keys into getter/setters that
- * collect dependencies and dispatch updates.
+ * 观察者类，会被附加到每个被观察的对象上，value.__ob__ = this
+ * 而对象的各个属性则会被转换成 getter/setter，并收集依赖和通知更新
  */
 export class Observer {
   value: any;
@@ -41,25 +39,36 @@ export class Observer {
 
   constructor(value: any) {
     this.value = value
+    // 实例化一个 dep，用于管理
     this.dep = new Dep()
     this.vmCount = 0
+    // 在 value 对象上设置 __ob__ 属性
     def(value, '__ob__', this)
     if (Array.isArray(value)) {
+      /**
+      * value 为数组
+      * hasProto = '__proto__' in {}
+      * 用于判断对象是否存在 __proto__ 属性，通过 obj.__proto__ 可以访问对象的原型链
+      * 但由于 __proto__ 不是标准属性，所以有些浏览器不支持，比如 IE6-10，Opera10.1
+      * 为什么要判断，是因为一会儿要通过 __proto__ 操作数据的原型链
+      * 覆盖数组默认的七个原型方法，以实现数组响应式
+      */
       if (hasProto) {
+        // 有 __proto__
         protoAugment(value, arrayMethods)
       } else {
         copyAugment(value, arrayMethods, arrayKeys)
       }
       this.observeArray(value)
     } else {
+      // value 为对象，为对象的每个属性（包括嵌套对象）设置响应式
       this.walk(value)
     }
   }
 
   /**
-   * Walk through all properties and convert them into
-   * getter/setters. This method should only be called when
-   * value type is Object.
+   * 遍历对象上的每个 key，为每个 key 设置响应式
+   * 仅当值为对象时才会走这里
    */
   walk (obj: Object) {
     const keys = Object.keys(obj)
@@ -69,7 +78,7 @@ export class Observer {
   }
 
   /**
-   * Observe a list of Array items.
+   * 遍历数组，为数组的每一项设置观察，处理数组元素为对象的情况
    */
   observeArray (items: Array<any>) {
     for (let i = 0, l = items.length; i < l; i++) {
@@ -108,11 +117,13 @@ function copyAugment (target: Object, src: Object, keys: Array<string>) {
  * @param {*} value 对象 => {}
  */
 export function observe (value: any, asRootData: ?boolean): Observer | void {
+  // 非对象和 VNode 实例不做响应式处理
   if (!isObject(value) || value instanceof VNode) {
     return
   }
   let ob: Observer | void
   if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
+    // 如果 value 对象上存在 __ob__ 属性，则表示已经做过观察了，直接返回 __ob__ 属性
     ob = value.__ob__
   } else if (
     shouldObserve &&
@@ -121,6 +132,7 @@ export function observe (value: any, asRootData: ?boolean): Observer | void {
     Object.isExtensible(value) &&
     !value._isVue
   ) {
+    // 创建观察者实例
     ob = new Observer(value)
   }
   if (asRootData && ob) {
